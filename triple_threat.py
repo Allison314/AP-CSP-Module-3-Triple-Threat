f10d66d3f05f70802aeb8841fde9c7341867ab02
Allison314 <avh80027@gmail.com>
2024-11-14 22:35:27 +0000

    scary save

--- triple_threat.py
+++ triple_threat.py
@@ -1,20 +1,25 @@
 """
 Triple threat simulation game for AP Computer Science Principles.
 Monarch High School - Boulder Valley School District
-Name - Month Year
+Allison Van Houten - 11/24
 """
 
 import random
 
 def main() -> None:
-    pass # remove
     # set variables for cost to play and base prize
+cost_to_play: int = 1
+base_price: int = 10
 
     # roll three dice
+role_1: int = random.radint(1, 6)
+role_2: int = random.radint(1, 6)
+role_3: int = random.radint(1, 6) 
 
-    # check if they are equal   
+    # check if they are equal  
+if role_1 == role_2 and role_2 == role_3 
+    
     # if they are, calculate the prize
-
     # output results
 
 if __name__ == "__main__":
